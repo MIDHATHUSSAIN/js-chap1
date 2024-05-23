7f4845f31a19bbf27921469eb3195cdd270eee11
MIDHAT-HUSSAIN <135623033+MIDHATHUSSAIN@users.noreply.github.com>
2024-05-23 13:53:07 +0500

    Add files via upload

---
+++ alert.js
@@ -0,0 +1,16 @@
+
+alert('Welcome to our website!'); /* Question 1: Write a script to greet your website visitor*/
+
+alert('Error! Please enter a valid password.') /* Question 2: */
+
+alert('Welcome to JS Land...\nHappy Coding!'); /*Question 3: */
+
+alert('Welcome to JS Land');
+alert('prevent this page from creating additional dialogs.'); /*Question 4: */
+
+
+let message=["Hello...I can run JS through my web browser's console"]
+
+console.log(message); /*Question 5: */
+
+
